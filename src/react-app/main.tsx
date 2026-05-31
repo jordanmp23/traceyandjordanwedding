1cd01f774eb7e1136b0956f09121f9a6931b7f57
Jordan Palmer <jor.palmer97@gmail.com>
2026-05-31 17:48:58 -0400

    hide webpage

--- src/react-app/main.tsx
+++ src/react-app/main.tsx
@@ -2,14 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import App from "./App.tsx";
-// import ComingSoon from "./pages/ComingSoon.tsx";
+// import App from "./App.tsx";
+import ComingSoon from "./pages/ComingSoon.tsx";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<BrowserRouter>
-			<App />
-			{/* <ComingSoon /> */}
+			{/* <App /> */}
+			<ComingSoon />
 		</BrowserRouter>
 	</StrictMode>,
 );
