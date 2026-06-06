53f2d1a1bed6b38b55ff646d232b93822cd15212
Jordan Palmer <jor.palmer97@gmail.com>
2026-06-06 13:43:10 -0400

    our story img

--- src/react-app/pages/OurStory.tsx
+++ src/react-app/pages/OurStory.tsx
@@ -1,10 +1,10 @@
 import PageHero from "../components/page_hero_components/page_hero";
-import eventPageImg  from "../assets/engagement_walking.jpg"
+import ourStoryImg  from "../assets/vt_grad_pic.jpg"
 
 function OurStory() {
 	return (
 		<>
-			<PageHero title="Our Story" image={eventPageImg} position="85% 20%" />
+			<PageHero title="Our Story" image={ourStoryImg} position="85% 20%" />
 			<section className="page">
 				<div className="page-inner narrow">
 					<h2 className="page-title">Our Story</h2>
