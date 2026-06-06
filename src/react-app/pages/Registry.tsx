3baf6f1129f37ebd86c5e41fd80c8611b4c7bf77
Jordan Palmer <jor.palmer97@gmail.com>
2026-06-06 13:25:43 -0400

    registry img

--- src/react-app/pages/Registry.tsx
+++ src/react-app/pages/Registry.tsx
@@ -1,10 +1,10 @@
 import PageHero from "../components/page_hero_components/page_hero";
-import eventPageImg  from "../assets/engagement_walking.jpg"
+import registryImg  from "../assets/engagement_hug.jpg"
 
 function FAQ() {
 	return (
 		<>
-			<PageHero title="Registry" image={eventPageImg} position="85% 20%" />		
+			<PageHero title="Registry" image={registryImg} position="85% 20%" />		
 			<section className="page">
 				<div className="page-inner">
 					<h2 className="page-title">Registry</h2>
