0674db7146e068613ce96ac3e7aa29b7a68aa32f
Jordan Palmer <jor.palmer97@gmail.com>
2026-09-19 16:08:28 -0400

    remove coming soon

--- src/react-app/main.tsx
+++ src/react-app/main.tsx
@@ -1,25 +1,10 @@
-import { StrictMode, useState } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, useNavigate } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
-import ComingSoon from "./pages/ComingSoon.tsx";
-import { isAuthed } from "./lib/auth";
 
 function Root() {
-	const [authed, setAuthed] = useState(isAuthed);
-	const navigate = useNavigate();
-
-	if (!authed) {
-		return (
-			<ComingSoon
-				onUnlock={() => {
-					setAuthed(true);
-					navigate("/", { replace: true });
-				}}
-			/>
-		);
-	}
 
 	return <App />;
 }
